a77080b47124760756fc0b91fbe23b09b26c3038
Christian Dickert <Christian.Dickert@tomtom.com>
2018-11-15 14:44:33 +0100

    Init

---
+++ App.js
@@ -0,0 +1,72 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, Button} from 'react-native';
+import Countly from 'countly-sdk-react-native';
+import ToastExample from './NativeModules';
+
+Countly.isDebug = true; 
+Countly.begin("http://server.ip","xxx","deviceId")
+  .then((result) => {
+    // do your stuff on session start
+  })
+  .catch((err) => {
+    // returns cause of failed initialization
+  });
+Countly.enableCrashReporting(true);
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+const crashNative = () => {
+    ToastExample.show()
+}
+
+const crashJS = () => {
+  const array = [undefined];
+  const i = array[0].bla
+}
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+        <Button title="Crash me Native!" onPress={crashNative} />
+        <Button title="Crash me JS!" onPress={crashJS} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
